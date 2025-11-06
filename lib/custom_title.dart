15ce31ff38241abd1a95245f366152c528a699bf
Niholabonu <161412299+Nihola@users.noreply.github.com>
2025-11-06 22:54:39 +0500

    Create custom_title.dart

---
+++ lib/custom_title.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomTitle extends StatelessWidget {
+  final String text;
+  final Color color;
+
+  const CustomTitle({required this.text, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: color),
+    );
+  }
+}
