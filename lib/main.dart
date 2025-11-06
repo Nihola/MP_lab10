c038667a6e5ae51af3f19f319434bc5e15bb2e5c
Niholabonu <161412299+Nihola@users.noreply.github.com>
2025-11-06 22:44:33 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,68 @@
 import 'package:flutter/material.dart';
+import 'screens/widget_demo.dart';
+import 'screens/custom_title_demo.dart';
+import 'screens/custom_counter_demo.dart';
+import 'screens/combined_widgets.dart';
+import 'screens/animated_container.dart';
+import 'screens/logo_animation.dart';
+import 'screens/gesture_box.dart';
+import 'screens/theme_switcher.dart';
+import 'screens/animated_list_demo.dart';
+import 'screens/mini_project_home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter UI Assignment',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(title: 'Assignment Navigator'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> pages = [
+      {'title': '1. Widgets Demo', 'widget': WidgetDemoPage()},
+      {'title': '2. CustomTitle Demo', 'widget': CustomTitleDemo()},
+      {'title': '3. CustomCounter Demo', 'widget': CustomCounterDemo()},
+      {'title': '4. Combined Widgets', 'widget': CombinedWidgetsPage()},
+      {'title': '5. AnimatedContainer', 'widget': AnimatedBox()},
+      {'title': '6. Logo Animation', 'widget': LogoAnimation()},
+      {'title': '7. GestureDetector', 'widget': GestureBox()},
+      {'title': '8. Theme Switcher', 'widget': ThemeSwitcherApp()},
+      {'title': '9. AnimatedList', 'widget': AnimatedListDemo()},
+      {'title': '10. Mini Project', 'widget': MiniProjectHome()},
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      appBar: AppBar(title: Text(title)),
+      body: ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemCount: pages.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => pages[index]['widget']),
+                );
+              },
+              child: Text(pages[index]['title']),
+            ),
+          );
+        },
       ),
     );
   }
